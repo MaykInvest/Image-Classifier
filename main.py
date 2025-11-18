8d943017ceaaefa0f09a9902671be033f4f4e78d
Maykon Jonattan <maykinvest@outlook.com>
2025-11-18 12:07:46 -0300

    create the function to classify image

--- main.py
+++ main.py
@@ -40,4 +40,20 @@ def preprocess_image(image):
     return img
 
 
+def classify_image(model, image):
+    try:
+        processed_image = preprocess_image(image)
+        predictions = model.predict(processed_image)
+        decoded_predictions = decode_predictions(predictions, top=3)[0]
+
+        return decoded_predictions
+    except Exception as e:
+        st.error(f"Error classifying image: {str(e)}")
+        return None
+
+
+
+
+
+
 
